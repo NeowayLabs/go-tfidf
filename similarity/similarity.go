5699adfb958516f64bc4c2fca6d14c0b1a5bc247
Victor <victor.vieira@neoway.com.br>
2021-03-29 17:17:44 -0300

    Started implementation for the TfIdf struct and a few more complementary functions. Also added similarity module

---
+++ similarity/similarity.go
@@ -0,0 +1,30 @@
+package similarity
+
+import "math"
+
+func Cosine(a []float64, b []float64) float64 {
+	if len(a) != len(b) {
+		return -1.0
+	}
+
+	return ProductDot(a, b) / (VectorMagnitude(a) * VectorMagnitude(b))
+}
+
+func VectorMagnitude(vector []float64) float64 {
+	squareSums := 0.0
+
+	for _, v := range vector {
+		squareSums += v * v
+	}
+
+	return math.Sqrt(squareSums)
+}
+
+func ProductDot(vectorA []float64, vectorB []float64) float64 {
+	result := 0.0
+	for i := 0; i < len(vectorA); i++ {
+		result += vectorA[i] * vectorB[i]
+	}
+
+	return result
+}
